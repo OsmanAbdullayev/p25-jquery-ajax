03fd0f4b8cb6fa54288132cbcb7730dd04d4b87f
Osman Abdullayev <os.abdullayev@gmail.com>
2022-10-08 02:24:34 +0400

    before uploading to GitHub

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,21 @@
 $(document).ready(function () {
-	$(".toggle").click(function () {
-		$("nav#colors").toggleClass("shown");
+    $(document).bind('keypress', function(e) {
+        if(e.keyCode==13){
+             $('#button').trigger('click');
+         }
+    });
+
+
+	$("#button").click(function () {
+		let inputValue = $("#search").val();
+		$("#textOutput").load(`./assets/data/${inputValue.toLowerCase()}.txt`, function (response, status, xhr) {
+			if (status == "error") {
+				var msg = "There was no match in our database. Please, make sure you typed the name of the country correctly.";
+				alert(msg);
+			} else {
+				$("h1").html(`This is what we found about ${inputValue.toUpperCase()}:`);
+                $("#search").val("");
+			}
+		});
 	});
-
-	$("#hamburger").click(function () {
-		$("aside.verticalMenu").toggleClass("showNav");
-		$(this).html() == '<i class="fa-solid fa-xmark"></i>'
-			? $(this).html('<i class="fa-solid fa-bars"></i>')
-			: $(this).html('<i class="fa-solid fa-xmark"></i>');
-	});
-});
-
-$("li.top").hover(function () {
-	$(".sub", this).slideToggle("fast");
-});
-
-$("li.topAside").click(function () {
-	$(".sub", this).slideToggle("fast");
-});
-
-$("button.black").click(function () {
-	$("body").removeClass().addClass("black");
-});
-
-$("button.blue").click(function () {
-	$("body").removeClass();
-});
-
-$("button.indigo").click(function () {
-	$("body").removeClass().addClass("indigo");
-});
-
-// 
-
-$("button.coral").click(function () {
-	$("body").removeClass().addClass("coral");
-});
-
-$("button.teal").click(function () {
-	$("body").removeClass().addClass("teal");
-});
-
-$("button.green").click(function () {
-	$("body").removeClass().addClass("green");
-});
-
-$("button.grey").click(function () {
-	$("body").removeClass().addClass("grey");
-});
-
-$("button.orange").click(function () {
-	$("body").removeClass().addClass("orange");
 });
